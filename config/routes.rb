7159dcc730efa5b3ed142d8fa3322a23b3f03f5f
Johanna <Johannakarlsson1@hotmail.com>
2018-02-28 15:26:28 -0500

    password doesn't match

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'register' => 'users/new'
+  root 'pages#home'
+
+  resources :users
+  get 'register' => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/user_registrations_spec.rb
+++ spec/features/user_registrations_spec.rb
@@ -1,19 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature "user registration", type: :feature do
+  before(:each) do
+    visit register_path
+  end
   it "successful registration" do
-    # visit the registration page
-    visit '/register'
-    # fill in the username and password
     fill_in 'Username', with: 'username'
     fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
-    # click submit
-    click_button 'Create'
-    # expect: to see homepage
-    expect(page).to have_content "Title"
-    # expect: to see welcome username
-    expect(page).to have_content "Welcome username"
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Create User'
+
+    ["Title", "Welcome username"].each do |phrase|
+      expect(page).to have_content phrase
+    end
   end
-  it "short password"
+
+  it "short password" do
+    fill_in 'Username', with: 'username'
+    fill_in 'Password', with: '123'
+    fill_in 'Password confirmation', with: '123'
+    click_button 'Create User'
+    expect(page).to have_content "Password is too short"
+  end
+
+  it "passwords don't match" do
+  fill_in 'Username', with: 'username'
+  fill_in 'Password', with: 'password'
+  fill_in 'Password confirmation', with: 'notmatching'
+  click_button 'Create User'
+  save_and_open_page
+  expect(page).to have_content "Password confirmation doesn't match Password"
+end
+
 end
