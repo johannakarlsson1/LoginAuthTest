e902f17de9c5a2b3467609ecd6d79974bc4e9cbf
Johanna <Johannakarlsson1@hotmail.com>
2018-02-28 14:17:09 -0500

    user model

--- spec/features/user_registrations_spec.rb
+++ spec/features/user_registrations_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "user registration", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "successful registration" do
+    # visit the registration page
+    visit '/register'
+    # fill in the username and password
+    fill_in 'Username', with: 'username'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    # click submit
+    click_button 'Create'
+    # expect: to see homepage
+    expect(page).to have_content "Title"
+    # expect: to see welcome username
+    expect(page).to have_content "Welcome username"
+  end
+  it "short password"
 end
